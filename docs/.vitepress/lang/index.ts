d2c483ce345b291c6da25c98c90b3aad4134cf86
Javion <javionlog@163.com>
2025-01-03 16:45:32 +0800

    docs: add css-property-compute-process

--- docs/.vitepress/lang/index.ts
+++ docs/.vitepress/lang/index.ts
@@ -24,6 +24,7 @@ export const nav: Record<string, string | undefined> = {
   'frontend/css/base/containing-block': '包含块',
   'frontend/css/base/inline-formatting-context': '行内格式化上下文',
   'frontend/css/base/block-formatting-context': '块级格式化上下文',
+  'frontend/css/base/css-property-compute-process': 'CSS 属性值计算过程',
   'frontend/css/base/box-model': '盒模型',
   'frontend/html/advance/how-browser-work': '浏览器工作原理',
   'frontend/javascript/base/inheritance-and-the-prototype-chain': '继承与原形链',
